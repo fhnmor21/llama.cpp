02814468d4f1a2f519c2863de1a57dc8a8951d64
FlavioM <fhnmor21@gmail.com>
2026-09-01 13:24:57 +1000

    updated models presets

--- models_preset.sh
+++ models_preset.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-./build_hip/bin/llama-server --models-preset models.ini  --models-max 1 --host 0.0.0.0 --port 8080 &
 ./build_hip/bin/llama-server --models-preset small_models.ini  --models-max 3 --host 0.0.0.0 --port 8088 &
+./build_hip/bin/llama-server --models-preset models.ini  --models-max 1 --host 0.0.0.0 --port 8080
+
